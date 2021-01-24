65154606049003e86a6c5a316d40eb4eac29d3f4
han1202012 <octopus_truth@163.com>
2021-01-25 00:23:54 +0800

    1

--- OpenGL/texture.cpp
+++ OpenGL/texture.cpp
@@ -1,10 +1,12 @@
 #include "texture.h"
+#include "utils.h"
 
 // 实现纹理初始化方法
 void Texture::Init(const char* imagePath) 
 {
 	// 从硬盘加载纹理文件到内存中
-
+	// 纹理图片数据放在 imageFileContent 指针指向的内存中
+	unsigned char* imageFileContent = LoadFileContent(imagePath);
 
 	// 解码图片
 
---
+++ OpenGL/utils.cpp
@@ -0,0 +1,20 @@
+#include "utils.h"
+
+// 加载文件方法声明
+unsigned char* LoadFileContent(const char* filePath)
+{
+	// 该指针指向的内存空间存储文件内容
+	unsigned char* fileContent = nullptr;
+
+	// 打开文件
+	FILE* pFile = fopen(filePath, "rb");
+
+	// 如果打开文件成功
+	if (pFile)
+	{
+
+	}
+
+
+	return fileContent;
+}
